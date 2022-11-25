4cf860f0436d72f5fba14ec45f257ae8cd4e91fe
SicdeX <sicdex@gmail.com>
2022-11-25 21:46:34 +0600

    see desc
    - add eslint
    - style code

--- Translate.ts
+++ Translate.ts
@@ -2,8 +2,7 @@ import { Menu, Utils } from "github.com/octarine-public/wrapper/index"
 
 const path = "github.com/octarine-public/resonant-pulse-abuse/scripts_files"
 const Load = (name: string) => {
-	return new Map<string, string>
-		(Object.entries(Utils.readJSON(`${path}/translate/${name}.json`)))
+	return new Map<string, string>(Object.entries(Utils.readJSON(`${path}/translate/${name}.json`)))
 }
 Menu.Localization.AddLocalizationUnit("russian", Load("ru"))
 Menu.Localization.AddLocalizationUnit("english", Load("en"))
